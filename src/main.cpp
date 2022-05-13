9ee1efffa2e68abebf7016de97e6adedd7090080
AaronTheNerd <bargeaaron@gmail.com>
2022-05-13 08:22:40 -0400

    Finished main function

--- src/main.cpp
+++ src/main.cpp
@@ -5,44 +5,55 @@
 
 #include <chrono> // std::chrono
 #include <iostream> // std::cout, std::endl
-#include "solution_generator.h"
-//#include "puzzle_generator.h"
-#include "sudoku.h"
+#include "solution_generator.h" // atn::SudokuSolutionGenerator
+#include "puzzle_generator.h" // atn::SudokuPuzzleGenerator
+#include "sudoku.h" // atn::Sudoku
 
 
 int main(int argc, char** argv) {
   // Instantiate variables for Sudoku Generation
   auto start = std::chrono::high_resolution_clock::now();
   auto end = std::chrono::high_resolution_clock::now();
-  atn::DIFFICULTY difficulty = atn::EASY;
+  std::chrono::duration<double, std::milli> ms_duration = end - start;
+  atn::DIFFICULTY difficulty = atn::SOLUTION;
 
   // Start generating valid Sudoku board
   start = std::chrono::high_resolution_clock::now();
-  atn::SudokuGenerator<GAME_SIZE> generator;
+  atn::SudokuSolutionGenerator<GAME_SIZE> generator;
   end = std::chrono::high_resolution_clock::now();
+
+  // Get the seed
+  unsigned seed = generator.get_seed();
 
   // Get the generated solution
   atn::Sudoku<GAME_SIZE> solution = generator.get_solution();
 
+  // Record seed
+  std::cout << "Seed: " << seed << std::endl;
+
   // Record execution time of solution
-  std::chrono::duration<double, std::milli> ms_duration = end - start;
-  std::cout << "Generated in " << ms_duration.count() << "ms" << std::endl;
+  ms_duration = end - start;
+  std::cout << "Solution generated in " << ms_duration.count() << "ms" << std::endl;
 
   // Start generating Sudoku puzzle
-  //atn::SudokuPuzzleGenerator<GAME_SIZE> puzzle_generator(difficulty, solution);
-
-  // Record seed
-  std::cout << "Seed: " << generator.get_seed() << std::endl;
+  start = std::chrono::high_resolution_clock::now();
+  atn::SudokuPuzzleGenerator<GAME_SIZE> puzzle_generator(difficulty, seed, solution);
+  end = std::chrono::high_resolution_clock::now();
 
   // Get generated Sudoku puzzle
-  //atn::Sudoku<GAME_SIZE> game = puzzle_generator.get_puzzle();
-
-  // Display Sudoku puzzle
-  //std::cout << "Puzzle:" << std::endl; 
-  //std::cout << game.to_str() << std::endl;
+  atn::Sudoku<GAME_SIZE> puzzle = puzzle_generator.get_puzzle();
 
   // Display Sudoku solution
-  std::cout << "\n\n\n\n\nSolution:" << std::endl;
-  std::cout << solution.to_str() << std::endl;
+  std::cout << "Solution:" << std::endl;
+  std::cout << solution.to_str() << "\n\n\n\n\n\n" << std::endl;
+
+  // Record execution time of puzzle
+  ms_duration = end - start;
+  std::cout << "Puzzle generated in " << ms_duration.count() << "ms" << std::endl;
+
+  // Display Sudoku puzzle
+  std::cout << "Puzzle:" << std::endl; 
+  std::cout << puzzle.to_str() << std::endl;
+
   return 0;
 }
