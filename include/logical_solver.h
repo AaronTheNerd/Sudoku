603160d46de9f530ec69220655e209bd38fb17e6
AaronTheNerd <bargeaaron@gmail.com>
2022-08-09 06:47:27 -0400

    Moved Technique Difficulty map to logical_solver.h

--- include/logical_solver.h
+++ include/logical_solver.h
@@ -18,11 +18,29 @@ struct LogicalSolverResults {
   uint16_t difficulty_score;
 };
 
+struct StepDifficulty {
+  uint16_t bonus;
+  uint16_t per_uses;
+};
+
 template <uint8_t T>
 atn::LogicalTechniquePtr<T> get_next_move(atn::Sudoku<T>&);
 
 template <uint8_t T>
 LogicalSolverResults<T> logical_solve(atn::Sudoku<T>&);
+
+// Constant variable
+
+static const std::unordered_map<LOGICAL_TECHNIQUE, StepDifficulty>
+    TECHNIQUE_DIFFICULTY = {{SINGLE_CANDIDATE, {0, 100}},
+        {SINGLE_POSITION, {0, 100}}, {CANDIDATE_LINES, {150, 200}},
+        {DOUBLE_PAIRS, {250, 250}}, {MULTIPLE_LINES, {300, 400}},
+        {NAKED_PAIR, {250, 500}}, {HIDDEN_PAIR, {300, 1200}},
+        {NAKED_TRIPLE, {600, 1400}}, {HIDDEN_TRIPLE, {800, 1600}},
+        {X_WING, {1200, 1600}}, {FORCING_CHAINS, {2100, 2100}},
+        {NAKED_QUAD, {1000, 4000}}, {HIDDEN_QUAD, {2000, 5000}},
+        {SWORDFISH, {2000, 6000}}};
+
 
 }  // namespace atn
 
