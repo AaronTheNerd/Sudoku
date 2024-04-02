9f06d35104e6eb47444281258abd3e655f64d82d
AaronTheNerd <bargeaaron@gmail.com>
2024-04-02 16:57:00 -0400

    Chore: Added tests for Cell functionality

--- src/test/cell.cxx
+++ src/test/cell.cxx
@@ -1,7 +1,42 @@
 #include <gtest/gtest.h>
+#include "cell.h"
 
-TEST(HelloTest, BasicAssertions) {
-  EXPECT_STRNE("hello", "world");
+TEST(CellTest, DefaultConstrucor) {
+  atn::sudoku::Cell cell;
+  EXPECT_EQ(cell.get(), atn::sudoku::UNSET_VALUE);
+}
 
-  EXPECT_EQ(7 * 6, 42);
+TEST(CellTest, Constructor) {
+  atn::sudoku::Cell cell{2};
+  EXPECT_EQ(cell.get(), 2);
+}
+
+TEST(CellTest, IsUnset) {
+  atn::sudoku::Cell cell;
+  EXPECT_FALSE(cell.is_set());
+}
+
+TEST(CellTest, IsSet) {
+  atn::sudoku::Cell cell{3};
+  EXPECT_TRUE(cell.is_set());
+}
+
+TEST(CellTest, SetValue) {
+  atn::sudoku::Cell cell;
+  cell.set(4);
+  EXPECT_TRUE(cell.is_set());
+  EXPECT_EQ(cell.get(), 4);
+}
+
+TEST(CellTest, SetNewValue) {
+  atn::sudoku::Cell cell{1};
+  cell.set(5);
+  EXPECT_TRUE(cell.is_set());
+  EXPECT_EQ(cell.get(), 5);
+}
+
+TEST(CellTest, UnsetValue) {
+  atn::sudoku::Cell cell{1};
+  cell.unset();
+  EXPECT_FALSE(cell.is_set());
 }
