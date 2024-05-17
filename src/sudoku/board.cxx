c2f438b33f219afc5daae233c7281fb682abf1ee
AaronTheNerd <bargeaaron@gmail.com>
2024-05-17 07:00:00 -0400

    Feature: Added Position parameter to Cell

--- src/sudoku/board.cxx
+++ src/sudoku/board.cxx
@@ -9,7 +9,7 @@ atn::sudoku::CellPtr atn::sudoku::Board::get(const atn::sudoku::Position& pos) {
 void atn::sudoku::Board::initialize_board() {
   for (uint y{0}; y < 9u; ++y) {
     for (uint x{0}; x < 9u; ++x) {
-      this->_board[y][x] = std::make_shared<atn::sudoku::Cell>();
+      this->_board[y][x] = std::make_shared<atn::sudoku::Cell>(atn::sudoku::Position{x, y});
     }
   }
 }
