691ccbd0255e89c453114f99b36274274c4e2b10
AaronTheNerd <bargeaaron@gmail.com>
2024-07-10 05:29:18 -0400

    Chore: Added additional test for the Single Position technique

--- src/test/single_position.cxx
+++ src/test/single_position.cxx
@@ -8,7 +8,7 @@ using namespace atn::sudoku;
 using namespace atn::test_utils;
 using namespace atn::generator::logic;
 
-TEST(SinglePositionTest, FullBoard) {
+TEST(SinglePositionTest, FullBoardColumn) {
   // clang-format off
   BoardPtr board = generate_board_with_options({
     {{4, {}},        {0, {1, 2, 5, 9}},    {6, {}},        {0, {1, 2, 9}},       {0, {1, 5, 9}},       {0, {2, 5, 7, 9}},       {0, {2, 7, 8}},          {3, {}},              {0, {2, 5, 7, 8}}},
@@ -32,6 +32,42 @@ TEST(SinglePositionTest, FullBoard) {
                                       {{4, 7}, {0, {3, 8}}},
                                       {{6, 7}, {0, {2, 3, 7, 8}}},
                                       {{8, 7}, {0, {2, 3, 5, 7, 8}}}};
+  SinglePosition move_candidate{board};
+  std::optional<NextMove> optional_move = move_candidate.get_next_move();
+  EXPECT_TRUE(optional_move.has_value());
+  NextMove move = optional_move.value();
+  CommandPtr command = move.get_command();
+  expect_board_state(board, before_state);
+  command->apply();
+  expect_board_state(board, after_state);
+  command->undo();
+  expect_board_state(board, before_state);
+}
+
+TEST(SinglePositionTest, FullBoardRow) {
+  // clang-format off
+  BoardPtr board = generate_board_with_options({
+    {{0, {2, 8}},          {6, {}},                 {0, {2, 5, 8}},          {3, {}},           {9, {}},              {1, {}},           {7, {}},        {0, {2, 5}},             {4, {}}},
+    {{0, {2, 3}},          {4, {}},                 {0, {2, 3, 5}},          {7, {}},           {6, {}},              {8, {}},           {0, {2, 3, 5}}, {0, {1, 2, 3, 5, 9}},    {0, {1, 2, 5, 9}}},
+    {{7, {}},              {0, {3, 9}},             {1, {}},                 {4, {}},           {0, {2, 5}},          {0, {2, 5}},       {0, {2, 3, 5}}, {8, {}},                 {6, {}}},
+    {{5, {}},              {0, {2, 3, 8, 9}},       {0, {2, 3, 6, 8, 9}},    {0, {1, 6, 8, 9}}, {4, {}},              {7, {}},           {0, {2, 3, 8}}, {0, {1, 2, 3, 6, 9}},    {0, {1, 2, 9}}},
+    {{4, {}},              {0, {3, 8, 9}},          {7, {}},                 {2, {}},           {0, {1, 3, 5, 8}},    {0, {3, 5, 6, 9}}, {0, {3, 5, 8}}, {0, {1, 3, 5, 6, 9}},    {0, {1, 5, 9}}},
+    {{0, {2, 3, 6, 8, 9}}, {1, {}},                 {0, {2, 3, 6, 8, 9}},    {0, {6, 8, 9}},    {0, {3, 5, 8}},       {0, {3, 5, 6, 9}}, {4, {}},        {0, {2, 3, 5, 6, 7, 9}}, {0, {2, 5, 7, 9}}},
+    {{0, {2, 3, 6, 8, 9}}, {0, {2, 3, 7, 8, 9}},    {0, {2, 3, 6, 7, 8, 9}}, {5, {}},           {0, {2, 3, 7, 8}},    {4, {}},           {1, {}},        {0, {2, 7}},             {0, {2, 7, 8}}},
+    {{0, {1, 2, 6, 8}},    {0, {2, 5, 7, 8}},       {0, {2, 5, 6, 7, 8}},    {0, {1, 6, 8}},    {0, {1, 2, 7, 8}},    {0, {2, 6}},       {9, {}},        {4, {}},                 {3, {}}},
+    {{0, {1, 2, 3, 8, 9}}, {0, {2, 3, 5, 7, 8, 9}}, {4, {}},                 {0, {1, 8, 9}},    {0, {1, 2, 3, 7, 8}}, {0, {2, 3, 9}},    {6, {}},        {0, {2, 5, 7}},          {0, {2, 5, 7, 8}}}
+  });
+  // clang-format on
+  std::vector<BoardState> before_state{{{1, 2}, {0, {3, 9}}},
+                                       {{1, 3}, {0, {2, 3, 8, 9}}},
+                                       {{1, 4}, {0, {3, 8, 9}}},
+                                       {{1, 6}, {0, {2, 3, 7, 8, 9}}},
+                                       {{1, 8}, {0, {2, 3, 5, 7, 8, 9}}}};
+  std::vector<BoardState> after_state{{{1, 2}, {9, {}}},
+                                      {{1, 3}, {0, {2, 3, 8}}},
+                                      {{1, 4}, {0, {3, 8}}},
+                                      {{1, 6}, {0, {2, 3, 7, 8}}},
+                                      {{1, 8}, {0, {2, 3, 5, 7, 8}}}};
   SinglePosition move_candidate{board};
   std::optional<NextMove> optional_move = move_candidate.get_next_move();
   EXPECT_TRUE(optional_move.has_value());
