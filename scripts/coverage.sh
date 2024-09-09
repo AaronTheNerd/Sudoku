9777c20422449b85101ef29471f9c42031a04b87
AaronTheNerd <bargeaaron@gmail.com>
2024-09-09 05:43:49 -0400

    Chore: Renamed coverage directory scripts

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -7,7 +7,7 @@ lcov -c -i -d .. -o base.info
 lcov -c -d .. -o test.info
 lcov -a base.info -a test.info -o total.info
 lcov --remove total.info -o filtered.info '/usr/*' '*build/_deps*' '*src/test/*' '*src/main.cxx'
-genhtml filtered.info -o ../coverage/output
-gcovr --root .. --filter ../src/ --exclude ../src/test/ --exclude ../src/main.cxx --html ../coverage/output/gcovr.html
-google-chrome ../coverage/output/gcovr.html
-google-chrome ../coverage/output/index.html
+genhtml filtered.info -o ../scripts/output
+gcovr --root .. --filter ../src/ --exclude ../src/test/ --exclude ../src/main.cxx --html ../scripts/output/gcovr.html
+google-chrome ../scripts/output/gcovr.html
+google-chrome ../scripts/output/index.html
